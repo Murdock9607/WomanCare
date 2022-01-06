ed4d2ce11ef2537bdf569ee7d55738b846e47b01
Angel Arturo <angelfaridarturo@gmail.com>
2022-01-06 09:39:27 -0500

    menu deslegable y cerrar sesion

--- app/src/main/java/Aplimovil/womancare/HomeActivity.java
+++ app/src/main/java/Aplimovil/womancare/HomeActivity.java
@@ -3,6 +3,7 @@ package Aplimovil.womancare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,9 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
     public Spinner menuSpinner;
@@ -22,19 +26,32 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         menuSpinner=findViewById(R.id.menuspiner);
+        ArrayList<String> listaOpciones = new ArrayList<>();
+        listaOpciones.add("Cerrar sesion");
+        listaOpciones.add(("Quienes somos"));
+        listaOpciones.add("Opciones");
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.Opciones, android.R.layout.simple_spinner_item);
         menuSpinner.setAdapter(adapter);
         menuSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+                if(parent.getLastVisiblePosition()==1) {
+                    Toast.makeText(parent.getContext(),
+                            "Seleccionado: " + listaOpciones.get(0).toString(),
+                            Toast.LENGTH_LONG).show();
+                    FirebaseAuth.getInstance().signOut();
+                    Intent i = new Intent(getApplicationContext(), IniciarSesionActivity.class);
+                    startActivity(i);
+                    parent.setSelection(0);
+                }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView)
             {
-                //nothing
+
             }
         });
 
