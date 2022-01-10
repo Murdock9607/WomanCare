d1c66bf28c18ca42feb19a912b7759159c4f0f4e
Natalia <natalianoguera@unicauca.edu.co>
2022-01-10 10:21:44 -0500

    PantallaCabello

---
+++ app/src/main/java/Aplimovil/womancare/HairCare1.java
@@ -0,0 +1,14 @@
+package Aplimovil.womancare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HairCare1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hair_care1);
+    }
+}
